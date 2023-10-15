88489dbcdaac346e8b324725bb6c9d166800de15
Maksim <max.godnavi133@gmail.com>
2023-10-15 05:20:50 +0300

    task 2 refactor done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ContactList } from 'components/contactList/ContactList';
 import { ContactForm } from 'components/contactForm/ContactForm';
 import { Filter } from 'components/filter/Filter';
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+export function App() {
+  const [contacts, setContacts] = useState(() =>
+    JSON.parse(
+      window.localStorage.getItem('contacts') ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+    )
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const localContacts = window.localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(localContacts);
 
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      setContacts(parsedContacts);
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContact = contactData => {
+  const addContact = contactData => {
     if (
-      this.state.contacts.some(
+      contacts.some(
         contact =>
           contact.name.toLocaleLowerCase() ===
           contactData.name.toLocaleLowerCase()
@@ -39,48 +40,34 @@ export class App extends Component {
       alert(`${contactData.name} is already in contacts.`);
       return;
     }
-    this.setState(prevState => {
-      return {
-        contacts: [...prevState.contacts, contactData],
-      };
-    });
+    setContacts(prevState => [...prevState, contactData]);
   };
 
-  removeContacts = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
-      };
-    });
+  const removeContacts = id => {
+    setContacts(prevState => prevState.filter(contact => contact.id !== id));
   };
 
-  handlFiltration = event => {
-    this.setState({
-      filter: event.target.value,
-    });
+  const handlFiltration = event => {
+    setFilter(event.target.value);
   };
 
-  filteredContacts = () => {
-    const normalizedContacts = this.state.filter.toLocaleLowerCase();
-    return this.state.contacts.filter(contact => {
+  const filteredContacts = () => {
+    const normalizedContacts = filter.toLocaleLowerCase();
+    return contacts.filter(contact => {
       return contact.name.toLocaleLowerCase().includes(normalizedContacts);
     });
   };
 
-  render() {
-    const afterFiltration = this.filteredContacts();
-    return (
-      <div>
-        <h1>Phonebook</h1>
-        <ContactForm
-          addContact={this.addContact}
-          globalContacts={this.state.contacts}
-        />
+  const afterFiltration = filteredContacts();
 
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onChange={this.handlFiltration} />
-        <ContactList contacts={afterFiltration} onDelit={this.removeContacts} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} globalContacts={contacts} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handlFiltration} />
+      <ContactList contacts={afterFiltration} onDelit={removeContacts} />
+    </div>
+  );
 }
